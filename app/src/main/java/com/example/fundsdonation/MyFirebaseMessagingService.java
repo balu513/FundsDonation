612d27418ce2337d79bbe31e5b383640a441f35b
balakrishna <balub513@gmail.com>
2019-02-19 18:29:27 +0530

    Added Notification Builder

--- app/src/main/java/com/example/fundsdonation/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/fundsdonation/MyFirebaseMessagingService.java
@@ -1,9 +1,19 @@
 package com.example.fundsdonation;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.example.fundsdonation.view.MainActivity;
+import com.example.fundsdonation.view.PaymentActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -53,7 +63,43 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         @Override
         public void run() {
             Toast.makeText(getApplicationContext(), remoteMessage.getNotification().getBody()+"",Toast.LENGTH_SHORT).show();
+            showNotification();
 
         }
     };
+
+    private void showNotification(){
+         NotificationManager mNotificationManager;
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext(), "notify_001");
+        Intent ii = new Intent(getApplicationContext(), MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, ii, 0);
+
+        NotificationCompat.BigTextStyle bigText = new NotificationCompat.BigTextStyle();
+        bigText.bigText("");
+        bigText.setBigContentTitle("Bidding Response");
+        bigText.setSummaryText("Successfull");
+
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher_round);
+        mBuilder.setContentTitle("Dear Balu");
+        mBuilder.setContentText("YOu WON BID");
+        mBuilder.setPriority(Notification.PRIORITY_MAX);
+        mBuilder.setStyle(bigText);
+
+        mNotificationManager =
+                (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "YOUR_CHANNEL_ID";
+            NotificationChannel channel = new NotificationChannel(channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            mNotificationManager.createNotificationChannel(channel);
+            mBuilder.setChannelId(channelId);
+        }
+
+        mNotificationManager.notify(0, mBuilder.build());
+    }
 }
