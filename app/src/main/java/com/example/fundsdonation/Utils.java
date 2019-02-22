9fc98b1aa8c42b500452443bbc89c3464a3ab06c
balakrishna <balub513@gmail.com>
2019-02-22 11:50:56 +0530

    conflicts test

--- app/src/main/java/com/example/fundsdonation/Utils.java
+++ app/src/main/java/com/example/fundsdonation/Utils.java
@@ -3,6 +3,6 @@ package com.example.fundsdonation;
 public class Utils {
     public Utils() {
         System.out.println("Constructor Called");
-        float pi=3.14;
+        float pi= 3.14f;
     }
 }
